6ef65b5d869c845a38a92cd9e99dd845ed082d4d
Sean Bevard <seanbevard@gmail.com>
2020-01-20 20:56:01 -0500

    finished up todo list

--- src/toDoList/toDoList.java
+++ src/toDoList/toDoList.java
@@ -8,17 +8,11 @@ public class toDoList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//int to store menu choice
 		int menuOption = -1;		
 		
+		//array to store items
 		ArrayList<toDoItem> toDoList = new ArrayList();
-		
-		toDoItem newItem = new toDoItem();
-		newItem.text = "First Item";
-		toDoList.add(newItem);
-
-		toDoItem newItem2 = new toDoItem();
-		newItem2.text = "second Item";
-		toDoList.add(newItem2);
 		
 		System.out.println("Welcome To Your To-Do List!\n"
 				+ "Please Select An Option:\n"
@@ -35,20 +29,37 @@ public class toDoList {
 			switch (menuOption) {
 				case 1:
 					//view list
-					System.out.println(toDoList.size());
-					for(int i = 0; i < toDoList.size(); i++) {
-						int itemNumber = i+1;
-						System.out.println(itemNumber + ": " + toDoList.get(i).text);
+					if (toDoList.size() == 0) {
+						System.out.println("ToDo List is Empty!");
+					} else {
+						for(int i = 0; i < toDoList.size(); i++) {
+							int itemNumber = i+1;
+							System.out.println(itemNumber + ": " + toDoList.get(i).text);
+						}
 					}
 					break;
 				case 2:
-					//add item
+					//Add ToDo Item
+					System.out.println("Enter your todo Item: ");
+					Scanner scan2 = new Scanner(System.in);
+					String input = scan2.nextLine();
+					toDoItem nextItem = new toDoItem();
+					nextItem.text = input;
+					toDoList.add(nextItem);
+					System.out.println("Item added!");
 					break;
 				case 3:
 					//remove item
+					System.out.println("Enter the ToDo Item Number You Want to Remove:");
+					Scanner scan3 = new Scanner(System.in);
+					int deleteItem = scan3.nextInt();
+					toDoList.remove(deleteItem-1);
+					System.out.println("Item Removed from list!");
 					break;
 				case 4:
 					//clear items
+					toDoList.removeAll(toDoList);
+					System.out.println("ToDo List Cleared!");
 					break;
 			}
 			System.out.println("Enter Another Menu Choice:");
